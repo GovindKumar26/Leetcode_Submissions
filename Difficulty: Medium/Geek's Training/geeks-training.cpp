b7ebf3daf50055f56343cd5e0dca700b5e6bbcf7
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-16 23:18:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -6,6 +6,31 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+  
+  int tab(int n, vector<vector<int>> points) {
+  vector<vector<int>> dp(n, vector<int>(4, -1));
+  dp[n - 1][0] = max(points[n - 1][1], points[n - 1][2]);
+  dp[n - 1][1] = max(points[n - 1][2], points[n - 1][0]);
+  dp[n - 1][2] = max(points[n - 1][0], points[n - 1][1]);
+  dp[n - 1][3] = max(points[n - 1][1], max(points[n - 1][2], points[n - 1][0]));
+
+  for (int day = n - 2; day >= 0; day--) {
+    for (int last = 3; last >= 0; last--) {
+      int maxi = 0;
+      dp[day][last] = 0;
+      for (int i = 0; i < 3; i++) {
+        if (i != last) {
+          int ans = points[day][i] + dp[day + 1][i];
+          maxi = max(maxi, ans);
+        }
+      }
+      dp[day][last] = maxi;
+    }
+  }
+
+  return dp[0][3];
+}
+  
   int solve(int n, vector<vector<int>>&points,int last,vector<vector<int>>&dp){
       if(n==0){
           int maximum = 0;
@@ -29,7 +54,8 @@ class Solution {
   }
     int maximumPoints(vector<vector<int>>& arr, int n) {
         vector<vector<int>>dp(n,vector<int>(4,-1));
-        return solve(n-1,arr,3,dp);
+       // return solve(n-1,arr,3,dp);
+       return tab(n,arr);
     }
 };
 
