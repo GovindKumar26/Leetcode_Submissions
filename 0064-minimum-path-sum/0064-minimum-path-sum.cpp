77780bf7b04c343a0ab4aba718e6bf5cf7b08fd3
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-17 15:52:11 +0530

    Time: 11 ms (18.6%), Space: 13.4 MB (12.13%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -33,11 +33,34 @@ public:
         return dp[m-1][n-1];
     }
 
+    int spaceOpt(int m, int n,vector<vector<int>>&obs){
+        vector<int>prev(n,0);
+
+        for(int i=0 ; i<m ; i++){
+            vector<int>curr(n,0);
+            for(int j=0 ; j<n ; j++){
+                 if(i==0 && j==0) curr[j] = obs[i][j];
+                 else{
+                  int  up = obs[i][j], left = obs[i][j];
+                    if(i>0) up = obs[i][j] + prev[j];
+                    else up = 1e9;
+                    if(j>0) left = obs[i][j] + curr[j-1];
+                    else left = 1e9;
+
+                    curr[j] = min(left,up);
+                 }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+    }
+
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
       vector<vector<int>>dp(m,vector<int>(n,-1));
        // return solve(m-1,n-1,grid,dp);
-       return tab(m,n,grid);
+      // return tab(m,n,grid);
+       return spaceOpt(m,n,grid);
     }
 };
