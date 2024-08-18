cc48f609c47c4ba72a4c72b8daad04c6122b71a1
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-18 15:14:23 +0530

    Time: 280 ms (27.27%), Space: 102.8 MB (16.66%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -17,6 +17,54 @@ bool func(int i, int target, vector<int>&arr,vector<vector<int>>&dp){
 
         return dp[i][target] = pick | notPick;
 }
+
+bool tab(int target, vector<int>&arr){
+    int n = arr.size();
+   vector<vector<bool>>dp(n,vector<bool>(target+1));
+    
+    for(int i=0 ; i<n ; i++) dp[i][0] = 1;
+    dp[0][arr[0]] = 1;
+    
+     for(int i=1 ; i<n ; i++){
+         for(int tar = 1 ; tar<=target ; tar++){
+             
+        bool notPick = dp[i-1][tar];
+        bool pick = false;
+        if(arr[i]<=tar) {
+            pick = dp[i-1][tar-arr[i]];
+        }
+
+        dp[i][tar] = pick | notPick;
+         }
+     }
+      return dp[n-1][target];
+     
+}
+
+bool spaceOpt(int target,vector<int>&arr){
+    int n = arr.size();
+    vector<bool>prev(target+1,0);
+    prev[0] = 1;
+   if(arr[0]<=target) prev[arr[0]] = 1;
+    
+    for(int i=1 ; i<n ; i++){
+        vector<bool>curr(target+1,0);
+        curr[0] = 1;
+        for(int tar =1 ; tar<=target ; tar++){
+            bool notPick = prev[tar];
+            bool pick = false;
+            if(arr[i]<=tar){
+                pick = prev[tar-arr[i]];
+            }
+            
+            curr[tar] = pick | notPick;
+        }
+        prev = curr;
+    }
+    
+    return prev[target];
+}
+
     bool canPartition(vector<int>& nums) {
           int sum = 0;
           int n = nums.size();
@@ -24,6 +72,7 @@ bool func(int i, int target, vector<int>&arr,vector<vector<int>>&dp){
 		if(sum%2) return false;
 		int k = sum/2;
 	    vector<vector<int>>dp(n,vector<int>(k+1,-1));
-        return func(n-1,k,nums,dp);
+     //   return func(n-1,k,nums,dp);
+        return spaceOpt(k,nums);
     }
 };
