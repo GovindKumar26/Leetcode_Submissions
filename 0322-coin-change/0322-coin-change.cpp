cc80fdaba13f373619b5352b87a2f6174b679d2d
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-21 17:52:33 +0530

    Time: 39 ms (51.64%), Space: 82 MB (5.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -19,11 +19,42 @@ public:
           return dp[i][amt] = min(pick, notPick);
           
     }
+
+
+    int tab(vector<int>&coins, int amt){
+        int n = coins.size();
+        vector<vector<int>>dp(n, vector<int>(amt+1,-1));
+        
+        for(int a = 0 ; a<=amt ; a++){
+            if(a%coins[0]==0) dp[0][a] = a/coins[0];
+            else dp[0][a] = 1e9;
+        }
+
+        for(int i=1 ; i<n ; i++){
+            for(int a=0 ; a<=amt ; a++){
+              int notPick = 0 + dp[i-1][a];
+              int pick = 1e9;
+          if(a>=coins[i]){
+                pick = 1 + dp[i][a-coins[i]];
+          }
+        
+
+         dp[i][a] = min(pick, notPick);
+            }
+        }
+        return dp[n-1][amt];
+    }
+
+
+    
+
+
     
     int coinChange(vector<int>& coins, int amount) { 
          int n = coins.size();
          vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-         int ans = memo(n-1, coins,amount, dp);
+        // int ans = memo(n-1, coins,amount, dp);
+           int ans = tab(coins, amount);
          if(ans>=1e9) return -1;
          return ans;
     }
