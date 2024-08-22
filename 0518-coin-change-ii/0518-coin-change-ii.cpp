b8b9aba7fce5498935ee9cf6f6cefb6fc30ceda8
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-22 12:51:46 +0530

    Time: 6 ms (74.47%), Space: 20.2 MB (27.77%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,21 +2,25 @@ class Solution {
 public:
 
         int memo(int i, vector<int>&coins, int amt, vector<vector<int>>&dp){
+             if(amt==0) {
+            return 1;
+          }
           if(i==0){
-            if(amt%coins[0]==0) return 1;
+            if(amt%coins[0]==0) return 1; 
             else return 0;
           }
-          
+         
+
           if(dp[i][amt]!=-1) return dp[i][amt];
 
-          int notPick = 0 + memo(i-1, coins, amt, dp);
+          int notPick =  memo(i-1, coins, amt, dp);
           int pick = 0;
           if(amt>=coins[i]){
                 pick =  memo(i, coins, amt-coins[i], dp);
           }
         
 
-          return dp[i][amt] = pick+notPick;
+          return dp[i][amt] = pick + notPick;
           
     }
 
