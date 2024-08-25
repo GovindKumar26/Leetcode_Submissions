6c314404c411d165c4428f77a36fe65205717f4f
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-25 20:28:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum number of deletions and insertions./minimum-number-of-deletions-and-insertions..cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+		     int memo(int i1, int i2, string t1, string t2, vector<vector<int>>&dp){
+        if(i1<0 || i2<0) return 0;
+        
+        if(dp[i1][i2]!=-1) return dp[i1][i2];
+        int ans;
+        if(t1[i1]==t2[i2]){
+           ans = 1 + memo(i1-1, i2-1, t1, t2, dp);
+        } else
+           ans = max(memo(i1-1, i2, t1, t2, dp), memo(i1, i2-1, t1, t2, dp));
+
+          return dp[i1][i2] = ans;
+
+     }
+
+     int tab(string t1, string t2){
+         int size1 = t1.length(), size2 = t2.length();
+      vector<vector<int>>dp(size1+1, vector<int>(size2+1, 0));
+      for(int i=0 ; i<=size2 ; i++) dp[0][i] = 0;
+      for(int i=0 ; i<=size1 ; i++) dp[i][0] = 0;
+       int ans;
+      for(int i1=1 ; i1<=size1 ; i1++){
+        for(int i2 = 1 ; i2<=size2 ; i2++){
+            if(t1[i1-1]==t2[i2-1]){
+                dp[i1][i2] = 1 + dp[i1-1][i2-1];
+            } else{
+               dp[i1][i2] = max(dp[i1-1][i2], dp[i1][i2-1]);
+            }
+
+        }
+      }
+      return dp[size1][size2];
+
+     }
+
+
+     int spaceOpt(string t1, string t2){
+        int size1 = t1.length(), size2 = t2.length();
+        vector<int>prev(size2+1);
+        vector<int>curr(size2+1);
+         for(int i=0 ; i<=size2 ; i++) prev[i] = 0;
+     
+      for(int i1=1 ; i1<=size1 ; i1++){
+        for(int i2 = 1 ; i2<=size2 ; i2++){
+            if(t1[i1-1]==t2[i2-1]){
+              curr[i2] = 1 + prev[i2-1];
+            } else{
+              curr[i2] = max(prev[i2], curr[i2-1]);
+            }
+
+        }
+        prev = curr;
+      }
+      return prev[size2];
+     }
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    int size1 = str1.length(), size2 = str2.length();
+        vector<vector<int>>dp(size1, vector<int>(size2, -1));
+      //  int lcs = memo(size1-1, size2-1, str1, str2, dp);
+       // int lcs = return tab(str1, str2);
+           int lcs = spaceOpt(str1, str2);
+           
+           int ans = size1 - lcs + size2 - lcs;
+           return ans;
+	    
+	} 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
