c973333551733bfea57972cc3f4b3ddd991a02ce
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-26 03:36:17 +0530

    Time: 0 ms (100%), Space: 12.7 MB (63.83%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int memo(int i, int j, string s, string t, vector<vector<int>>&dp){
+    int memo(int i, int j, string &s, string &t, vector<vector<int>>&dp){
         if(i<0) return j+1;
         if(j<0) return i+1;
         
@@ -9,6 +9,7 @@ public:
             return dp[i][j] = 0 + memo(i-1, j-1, s, t, dp);
         } else return dp[i][j] =  1 + min(memo(i-1, j, s, t, dp), min(memo(i, j-1, s, t, dp), memo(i-1, j-1, s, t, dp)));
     }
+    
     
     int minDistance(string word1, string word2) {
         int n = word1.length(), m = word2.length();
