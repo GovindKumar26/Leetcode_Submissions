7cc765018f405a7e343e3b52d1a7066f10fe3ad1
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-26 17:14:36 +0530

    Time: 32 ms (68.36%), Space: 30.9 MB (41.38%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -29,7 +29,7 @@ public:
     }
 
     bool tab(string& s, string& p) {
-        int n = s.length(), m = p.length();
+        int n = p.length(), m = s.length();
         vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
         dp[0][0] = true;
 
@@ -38,8 +38,8 @@ public:
 
         for (int i = 1; i <= n; i++) {
             bool flag = true;
-            for (int a = 0; a <= i; a++) {
-                if (p[a] != '*')
+            for (int a = 1; a <= i; a++) {
+                if (p[a-1] != '*')
                     flag = false;
                 break;
             }
@@ -49,11 +49,11 @@ public:
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
 
-                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                if (s[j - 1] == p[i - 1] || p[i - 1] == '?') {
                     dp[i][j] = dp[i - 1][j - 1];
                 }
 
-                else if (p[j - 1] == '*') {
+                else if (p[i - 1] == '*') {
                     dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
                 }
 
@@ -69,6 +69,6 @@ public:
         int m = s.length(), n = p.length();
          vector<vector<int>>dp(n,vector<int>(m,-1));
          return memo(n-1, m-1, s, p, dp);
-        return tab(s, p);
+       // return tab(s, p);
     }
 };
