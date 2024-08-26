a9989809542ac9ae0fb9e0722643704c8e126287
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-26 17:07:26 +0530

    Time: 23 ms (88.3%), Space: 30.6 MB (46.1%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    bool memo(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
+        if (i < 0 && j < 0)
+            return true;
+        if (i < 0 && j >= 0)
+            return false;
+        if (i >= 0 && j < 0) {
+            for (int a = 0; a <= i; a++) {
+                if (p[a] != '*')
+                    return false;
+            }
+            return true;
+        }
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[j] == p[i] || p[i] == '?') {
+            return dp[i][j] = memo(i - 1, j - 1, s, p, dp);
+        }
+
+        if (p[i] == '*') {
+            return dp[i][j] =
+                       (memo(i - 1, j, s, p, dp) | memo(i, j - 1, s, p, dp));
+        }
+
+        return dp[i][j] = false;
+    }
+
+    bool tab(string& s, string& p) {
+        int n = s.length(), m = p.length();
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+        dp[0][0] = true;
+
+        for (int j = 1; j <= m; j++)
+            dp[0][j] = false;
+
+        for (int i = 1; i <= n; i++) {
+            bool flag = true;
+            for (int a = 0; a <= i; a++) {
+                if (p[a] != '*')
+                    flag = false;
+                break;
+            }
+            dp[i][0] = flag;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+
+                else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+
+                else
+                    dp[i][j] = false;
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    bool isMatch(string s, string p) {
+        int m = s.length(), n = p.length();
+         vector<vector<int>>dp(n,vector<int>(m,-1));
+         return memo(n-1, m-1, s, p, dp);
+        return tab(s, p);
+    }
+};
