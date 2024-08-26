9b924d0ffc136c1ae02906f12036354f3de668c4
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-26 17:14:54 +0530

    Time: 28 ms (78.08%), Space: 30.8 MB (43.51%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -28,42 +28,42 @@ public:
         return dp[i][j] = false;
     }
 
-    bool tab(string& s, string& p) {
-        int n = p.length(), m = s.length();
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-        dp[0][0] = true;
+    // bool tab(string& s, string& p) {
+    //     int n = p.length(), m = s.length();
+    //     vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+    //     dp[0][0] = true;
 
-        for (int j = 1; j <= m; j++)
-            dp[0][j] = false;
+    //     for (int j = 1; j <= m; j++)
+    //         dp[0][j] = false;
 
-        for (int i = 1; i <= n; i++) {
-            bool flag = true;
-            for (int a = 1; a <= i; a++) {
-                if (p[a-1] != '*')
-                    flag = false;
-                break;
-            }
-            dp[i][0] = flag;
-        }
+    //     for (int i = 1; i <= n; i++) {
+    //         bool flag = true;
+    //         for (int a = 1; a <= i; a++) {
+    //             if (p[a-1] != '*')
+    //                 flag = false;
+    //             break;
+    //         }
+    //         dp[i][0] = flag;
+    //     }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
+    //     for (int i = 1; i <= n; i++) {
+    //         for (int j = 1; j <= m; j++) {
 
-                if (s[j - 1] == p[i - 1] || p[i - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
+    //             if (s[j - 1] == p[i - 1] || p[i - 1] == '?') {
+    //                 dp[i][j] = dp[i - 1][j - 1];
+    //             }
 
-                else if (p[i - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                }
+    //             else if (p[i - 1] == '*') {
+    //                 dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+    //             }
 
-                else
-                    dp[i][j] = false;
-            }
-        }
+    //             else
+    //                 dp[i][j] = false;
+    //         }
+    //     }
 
-        return dp[n][m];
-    }
+    //     return dp[n][m];
+    // }
 
     bool isMatch(string s, string p) {
         int m = s.length(), n = p.length();
