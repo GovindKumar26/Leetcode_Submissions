0e7dd68a200da7a564ebb3b6909d6b4f113c5513
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-08-27 16:56:51 +0530

    Time: 728 ms (5.01%), Space: 341.6 MB (5.05%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int memo(int i, int buy, vector<int>& prices, int n, int cap,
-             vector<vector<vector<int>>>& dp) {
-        if (cap == 0)
-            return 0;
-        if (i == n)
-            return 0;
+    // int memo(int i, int buy, vector<int>& prices, int n, int cap,
+    //          vector<vector<vector<int>>>& dp) {
+    //     if (cap == 0)
+    //         return 0;
+    //     if (i == n)
+    //         return 0;
 
-        if (dp[i][buy][cap] != -1)
-            return dp[i][buy][cap];
+    //     if (dp[i][buy][cap] != -1)
+    //         return dp[i][buy][cap];
 
-        int profit = 0;
-        if (buy) {
-            profit = max(-prices[i] + memo(i + 1, 0, prices, n, cap, dp),
-                         memo(i + 1, 1, prices, n, cap, dp));
-        } else {
-            profit = max(prices[i] + memo(i + 1, 1, prices, n, cap - 1, dp),
-                         memo(i + 1, 0, prices, n, cap, dp));
-        }
+    //     int profit = 0;
+    //     if (buy) {
+    //         profit = max(-prices[i] + memo(i + 1, 0, prices, n, cap, dp),
+    //                      memo(i + 1, 1, prices, n, cap, dp));
+    //     } else {
+    //         profit = max(prices[i] + memo(i + 1, 1, prices, n, cap - 1, dp),
+    //                      memo(i + 1, 0, prices, n, cap, dp));
+    //     }
 
-        return dp[i][buy][cap] = profit;
-    }
+    //     return dp[i][buy][cap] = profit;
+    // }
 
     int tab(vector<int>& prices) {
         int n = prices.size();
@@ -48,6 +48,10 @@ public:
 
         return dp[0][1][2];
     }
+
+    // int spaceOpt(vector<int>&prices){
+
+    // }
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
