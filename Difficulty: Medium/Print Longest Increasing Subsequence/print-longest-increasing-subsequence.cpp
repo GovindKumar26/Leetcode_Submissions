9d2767da6aa42f0d1a97689a722f22db903e5a38
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-09-30 19:47:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
+++ Difficulty: Medium/Print Longest Increasing Subsequence/print-longest-increasing-subsequence.cpp
@@ -7,40 +7,34 @@ using namespace std;
 class Solution {
   public:
     vector<int> longestIncreasingSubsequence(int n, vector<int>& arr) {
-      
-      
-        vector<int>dp(n,1), hash(n, 0);
-        int maximum = 1;
-        int lastIndex = 0;
-        
-        for(int i=0 ; i<n ; i++){
-             hash[i] = i;
-            for(int j=0 ; j<i ; j++){
-                if(arr[j]<arr[i] &&  1+dp[j]>dp[i]){
-                    dp[i] =  1+dp[j];
-                    hash[i] = j;
-                }
-            }
-            if(maximum<dp[i]){
-                maximum = dp[i];
-                lastIndex = i;
-            }
-        }
-        
-        vector<int>temp;
-        temp.push_back(arr[lastIndex]);
-        
-        while(hash[lastIndex]!=lastIndex){
-            lastIndex = hash[lastIndex];
-            temp.push_back(arr[lastIndex]);
-        }
-        
-        reverse(temp.begin(), temp.end());
-        
-        return temp;
+       vector<int>dp(n,1);
+       vector<int>hash(n);
+       int maximum = 1;
+       int lastIndex = 0;
+       
+       for(int i=0 ; i<n ; i++){
+           hash[i] = i;
+           for(int prev=0 ; prev<i ; prev++){
+               if(arr[prev]<arr[i] && 1+dp[prev]>dp[i]){
+                   dp[i] = 1+dp[prev];
+                   hash[i] = prev;
+               }
+           }
+           if(dp[i]>maximum){
+               maximum = dp[i];
+               lastIndex = i;
+           }
+       }
+       
+       vector<int>temp;
+       temp.push_back(arr[lastIndex]);
+       while(hash[lastIndex]!=lastIndex){
+           lastIndex = hash[lastIndex];
+           temp.push_back(arr[lastIndex]);
+       }
+       reverse(temp.begin(), temp.end());
+       return temp;
     }
-
-    
 };
 
 //{ Driver Code Starts.
