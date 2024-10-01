ca1e21a74aad784faa3425c65a938fbe09d67834
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-01 17:38:03 +0530

    Time: 23 ms (79.83%), Space: 12.6 MB (18.68%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.cpp
@@ -13,12 +13,36 @@ public:
         } 
         return dp[i][j] = mini;
     }
+
+    int tab(int n, vector<int>&cuts){
+        int c = cuts.size();
+        cuts.push_back(n);
+        cuts.insert(cuts.begin(), 0);
+        sort(cuts.begin(), cuts.end());
+   
+        vector<vector<int>>dp(c+2,vector<int>(c+2,0));
+        for(int i=c ; i>=1; i--){
+            for(int j=1 ; j<=c ; j++){
+                  if(i>j) continue;         
+                int mini = 1e9;
+                for(int idx = i ; idx<=j ; idx++){
+                    int cost = cuts[j+1] - cuts[i-1] + dp[i][idx-1] + dp[idx+1][j];
+                    mini = min(mini, cost);
+                } 
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][c];
+    }
+
     int minCost(int n, vector<int>& cuts) {
+         return tab(n, cuts);
         cuts.push_back(n);
         cuts.insert(cuts.begin(), 0);
         sort(cuts.begin(), cuts.end());
         int c = cuts.size();
         vector<vector<int>>dp(c,vector<int>(c,-1));
-        return memo(1, c-2, cuts, dp);
+        //return memo(1, c-2, cuts, dp);
+       
     }
 };
