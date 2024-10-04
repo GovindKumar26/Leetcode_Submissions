9a6bf63a7b284035fd9b91a401c6af2c447efb93
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-04 19:25:25 +0530

    Time: 15 ms (51.98%), Space: 19.6 MB (27.67%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        // int xorResult = 0;
-        // for(auto i : nums){
-        //     xorResult = xorResult^i;
-        // }
-        // return xorResult;
+        int xorResult = 0;
+        for(auto i : nums){
+            xorResult = xorResult^i;
+        }
+        return xorResult;
 
 
         // mapping
-        map<long long, int> mp;
-        for(int i=0 ; i<nums.size() ; i++){
-            mp[nums[i]]++;
-        }
-       for(auto i : mp){
-        if(i.second==1) return i.first;
-       }
-       return 0;
+    //     map<long long, int> mp;
+    //     for(int i=0 ; i<nums.size() ; i++){
+    //         mp[nums[i]]++;
+    //     }
+    //    for(auto i : mp){
+    //     if(i.second==1) return i.first;
+    //    }
+    //    return 0;
     }
 };
