8646fb8dc33bf8afeb9a3ff6fb6b6d816357aa69
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-04 18:31:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Union of two arrays/union-of-two-arrays.cpp
+++ Difficulty: Basic/Union of two arrays/union-of-two-arrays.cpp
@@ -12,12 +12,62 @@ class Solution {
   public:
     // Function to return the count of number of elements in union of two arrays.
     int doUnion(vector<int> arr1, vector<int> arr2) {
-      set<int>st;
-      for(int i=0 ;i<arr1.size();i++) st.insert(arr1[i]);
-      for(int i=0 ; i<arr2.size();i++) st.insert(arr2[i]);
+        
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.begin(), arr2.end());
+        
+        vector<int>ans;
+        
+        int i = 0, j = 0;
+        int n1 = arr1.size(), n2 = arr2.size();
+        while(i<n1 && j<n2){
+            if(arr1[i]<=arr2[j]){
+                if(ans.size()==0 || ans.back()!=arr1[i]){
+                    ans.push_back(arr1[i]);
+                }
+                i++;
+            }
+            
+            else if(arr2[j]<=arr1[i]){
+                if(ans.size()==0 || ans.back()!=arr2[j]){
+                    ans.push_back(arr2[j]);
+                }
+                j++;
+            }
+        }
+        
+        while(i<n1){
+          
+                if(ans.size()==0 || ans.back()!=arr1[i]){
+                    ans.push_back(arr1[i]);
+                }
+                i++;
+        }
+        
+        while(j<n2){
+           
+                if(ans.size()==0 || ans.back()!=arr2[j]){
+                    ans.push_back(arr2[j]);
+                }
+                j++;
+        }
+        
+        return ans.size();
+        
+    
+        
+        
+    //   set<int>st;
+    //   for(auto i : arr1) st.insert(i);
+    //   for(auto i : arr2) st.insert(i);
       
-      return st.size();
-    }
+    //   vector<int>ans;
+    //   for(auto i : st){
+    //       ans.push_back(i);
+    //   }
+      
+    //   return ans.size();
+     }
 };
 
 //{ Driver Code Starts.
