85bd809e0ca61ce707b2aec530fce101fc2dd63a
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-04 19:05:42 +0530

    Time: 11 ms (81.55%), Space: 20.8 MB (37.23%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -15,14 +15,27 @@ public:
     // return 0;
 
     // sum method
-    int sum = 0;
+//     int sum = 0;
   
-    int n = nums.size();
-   int sumTot = n*(n+1)/2;
+//     int n = nums.size();
+//    int sumTot = n*(n+1)/2;
 
-    for(auto i : nums){
-        sum = i+sum;
+//     for(auto i : nums){
+//         sum = i+sum;
+//     }
+//     return sumTot-sum;
+//     }
+
+      // XOR method
+      
+      int n = nums.size();
+       int xor1= 0, xor2 =0;
+      for(int i=0 ; i<n ; i++){
+        xor1 = xor1^nums[i];
+        xor2 = xor2^(i+1);
+      }
+
+      return xor1^xor2;
     }
-    return sumTot-sum;
-    }
+
 };
