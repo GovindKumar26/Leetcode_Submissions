e3b9b72fc39b73a22361e0b33511baba18526643
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-07 16:11:29 +0530

    Time: 20 ms (39.22%), Space: 26.8 MB (92.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,41 +1,42 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int, int>mp;
-        int n  = nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        for(auto it : mp){
-            if(it.second>n/2) return it.first;
-        }
-        return -1;
+        // map<int, int>mp;
+        // int n  = nums.size();
+        // for(int i=0;i<n;i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto it : mp){
+        //     if(it.second>n/2) return it.first;
+        // }
+        // return -1;
 
         //   int n = nums.size();
         // sort(nums.begin(),nums.end());
         // return nums[n/2];
 
         // Moore's voting algorithm
-        // int cnt=0;
-        // int ele;
-        // for(int i=0 ; i<nums.size(); i++){
-        //     if(cnt==0){
-        //         ele = nums[i];
-        //         cnt==1;
-        //     } else if(nums[i]==ele){
-        //         cnt++;
-        //     } else{
-        //         cnt--;
-        //     }
-        // }
+        int cnt=0;
+        int ele;
+        for(int i=0 ; i<nums.size(); i++){
+            if(cnt==0){
+                ele = nums[i];
+                cnt=1;
+            } else if(nums[i]==ele){
+                cnt++;
+            } else{
+                cnt--;
+            }
+        }
+               
+       int cnt1 =0;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i]==ele) cnt1++;
+        }
 
-        // for(int i=0 ; i<nums.size() ; i++){
-        //     if(nums[i]==ele) cnt1++;
-        // }
-
-        // if(cnt1>nums.size()/2){
-        //     return ele;
-        // }
-        // return -1;
+        if(cnt1>nums.size()/2){
+            return ele;
+        }
+        return -1;
     }
 };
