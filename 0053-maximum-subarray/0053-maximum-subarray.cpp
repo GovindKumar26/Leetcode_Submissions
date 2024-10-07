08d3a1107e87110040a7d3976d7ba86f0fa0c1d1
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-07 20:03:48 +0530

    Time: 74 ms (71.27%), Space: 70.5 MB (71.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,24 +3,24 @@ public:
     int maxSubArray(vector<int>& nums) {
         //kodane's algotithm
 
-        // int n = nums.size();
-        // int maxEle = nums[0];
-        // int res = nums[0];
-        // for(int i=1 ; i<n ; i++){
-        //     maxEle = max(maxEle + nums[i], nums[i]);
-        //     res = max(res, maxEle);
-        // }
-        // return res;
+        int n = nums.size();
+        int maxEle = nums[0];
+        int res = nums[0];
+        for(int i=1 ; i<n ; i++){
+            maxEle = max(maxEle + nums[i], nums[i]);
+            res = max(res, maxEle);
+        }
+        return res;
   
-        int maxSum = INT_MIN;
-        int currentSum = 0;
+    //     int maxSum = INT_MIN;
+    //     int currentSum = 0;
 
-       for(int i=0 ; i<nums.size(); i++){
-        currentSum = currentSum + nums[i];
-        maxSum = max(maxSum, currentSum);
-        if(currentSum<0) currentSum=0;
-       }
-       return maxSum;
+    //    for(int i=0 ; i<nums.size(); i++){
+    //     currentSum = currentSum + nums[i];
+    //     maxSum = max(maxSum, currentSum);
+    //     if(currentSum<0) currentSum=0;
+    //    }
+    //    return maxSum;
 
     }
 };
