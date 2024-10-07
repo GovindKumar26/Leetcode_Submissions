d5e0c0edbe89bff3a8445e3b93a5393a93677ec2
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-07 16:09:57 +0530

    Time: 26 ms (10.6%), Space: 27.1 MB (49.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -10,5 +10,32 @@ public:
             if(it.second>n/2) return it.first;
         }
         return -1;
+
+        //   int n = nums.size();
+        // sort(nums.begin(),nums.end());
+        // return nums[n/2];
+
+        // Moore's voting algorithm
+        // int cnt=0;
+        // int ele;
+        // for(int i=0 ; i<nums.size(); i++){
+        //     if(cnt==0){
+        //         ele = nums[i];
+        //         cnt==1;
+        //     } else if(nums[i]==ele){
+        //         cnt++;
+        //     } else{
+        //         cnt--;
+        //     }
+        // }
+
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     if(nums[i]==ele) cnt1++;
+        // }
+
+        // if(cnt1>nums.size()/2){
+        //     return ele;
+        // }
+        // return -1;
     }
 };
