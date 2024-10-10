16307f2d0fe77c50b27b6cb8944217e99d8ca7c6
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-10 17:16:21 +0530

    Time: 12 ms (30.79%), Space: 20 MB (14.74%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,20 +2,35 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         //brute 
-        int n = nums.size();
-        vector<int>li;
+        // int n = nums.size();
+        // vector<int>li;
 
-        for(int i=0 ; i<n  ;i++){
-            if(li.size()==0 || li[0]!=nums[i]){
-                int cnt = 0;
-                for(int j=0 ; j<n ;j++)
-                    if(nums[i]==nums[j]) cnt++;
+        // for(int i=0 ; i<n  ;i++){
+        //     if(li.size()==0 || li[0]!=nums[i]){
+        //         int cnt = 0;
+        //         for(int j=0 ; j<n ;j++)
+        //             if(nums[i]==nums[j]) cnt++;
 
-                    if(cnt>n/3) li.push_back(nums[i]);
+        //             if(cnt>n/3) li.push_back(nums[i]);
                 
-            }
-            if(li.size()==2) break;
+        //     }
+        //     if(li.size()==2) break;
+        // }
+        // return li;
+
+        //hashmap
+        int n = nums.size();
+        unordered_map<int, int>mp;
+
+        for(int i=0 ; i<n ; i++){
+            mp[nums[i]]++;
         }
-        return li;
+        vector<int>ans;
+        for(auto m : mp){
+            if(m.second>n/3){
+                ans.push_back(m.first);
+            }
+        }
+        return ans;
     }
 };
