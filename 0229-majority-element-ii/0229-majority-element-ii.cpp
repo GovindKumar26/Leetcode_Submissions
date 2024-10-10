884b742346968d8a4531a0407c3668c1f6bac2d3
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-10-10 17:39:06 +0530

    Time: 14 ms (16.8%), Space: 18.7 MB (86.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -19,18 +19,53 @@ public:
         // return li;
 
         //hashmap
+        // int n = nums.size();
+        // unordered_map<int, int>mp;
+
+        // for(int i=0 ; i<n ; i++){
+        //     mp[nums[i]]++;
+        // }
+        // vector<int>ans;
+        // for(auto m : mp){
+        //     if(m.second>n/3){
+        //         ans.push_back(m.first);
+        //     }
+        // }
+        // return ans;
+
+
+        //Moore's voting algo for majority extended
+
+        int cnt1 = 0, cnt2 = 0;
+        int ele1=INT_MIN , ele2=INT_MIN;
         int n = nums.size();
-        unordered_map<int, int>mp;
 
         for(int i=0 ; i<n ; i++){
-            mp[nums[i]]++;
-        }
-        vector<int>ans;
-        for(auto m : mp){
-            if(m.second>n/3){
-                ans.push_back(m.first);
+            if(cnt1==0 && ele2!=nums[i]){
+                cnt1 = 1;
+                ele1 = nums[i];
+            } else if(cnt2==0 && ele1!=nums[i]){
+                cnt2 = 1;
+                ele2 = nums[i];
+            }
+            else if(ele1==nums[i]) cnt1++;
+            else if(ele2==nums[i]) cnt2++;
+            else{
+                cnt1--, cnt2--;
             }
         }
+     
+        vector<int>ans;
+        cnt1=0, cnt2=0;
+        for(int i=0 ; i<n ; i++){
+             if(nums[i]==ele1) cnt1++;
+             if(nums[i]==ele2) cnt2++;
+        }
+
+        if (cnt1 > n / 3) ans.push_back(ele1);
+        if (cnt2 > n / 3 && ele2 != ele1) ans.push_back(ele2);  
+
         return ans;
+        
     }
 };
