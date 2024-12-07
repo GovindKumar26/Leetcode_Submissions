4f82cfa4f80df0fcb86b410161ca9df29164cdfb
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-12-08 02:09:18 +0530

    Time: 3 ms (44.17%), Space: 15.8 MB (13.83%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -6,49 +6,51 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-         map<int,map<int,vector<int>>> nodes;
-        queue<pair<TreeNode*,pair<int,int>>> q;
-       vector<vector<int>>ans;
-        
-        if(root==NULL) return ans;
-        
-        q.push(make_pair(root,make_pair(0,0)));
-        
-        while(!q.empty()){
-            pair<TreeNode*,pair<int,int>> temp = q.front();
+        map<int, map<int, vector<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        vector<vector<int>> ans;
+
+        if (root == NULL)
+            return ans;
+
+        q.push(make_pair(root, make_pair(0, 0)));
+
+        while (!q.empty()) {
+            pair<TreeNode*, pair<int, int>> temp = q.front();
             q.pop();
-            
-            TreeNode*frontNode = temp.first;
+
+            TreeNode* frontNode = temp.first;
             int hd = temp.second.first;
             int lvl = temp.second.second;
-            
+
             nodes[hd][lvl].push_back(frontNode->val);
-            
-            if(frontNode->left){
-                q.push(make_pair(frontNode->left,make_pair(hd-1,lvl+1)));
+
+            if (frontNode->left) {
+                q.push(make_pair(frontNode->left, make_pair(hd - 1, lvl + 1)));
             }
-            
-              if(frontNode->right){
-                q.push(make_pair(frontNode->right,make_pair(hd+1,lvl+1)));
+
+            if (frontNode->right) {
+                q.push(make_pair(frontNode->right, make_pair(hd + 1, lvl + 1)));
             }
         }
-        
-        for(auto i: nodes){
-            vector<int>a;
-          for(auto j : i.second){
-            sort(j.second.begin(),j.second.end());
-              for(auto k : j.second){
-              a.push_back(k);
-             }
-          } 
-          ans.push_back(a);      
+
+        for (auto i : nodes) {
+            vector<int> a;
+            for (auto j : i.second) {
+                sort(j.second.begin(), j.second.end());
+                for (auto k : j.second) {
+                    a.push_back(k);
+                }
+            }
+            ans.push_back(a);
         }
-     return ans;  
+        return ans;
     }
 };
