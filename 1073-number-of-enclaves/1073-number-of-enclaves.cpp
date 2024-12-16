26b3373b88728060c5cfd2efb2df54f828684645
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-12-16 19:33:10 +0530

    Time: 11 ms (48.88%), Space: 31.9 MB (64.38%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-
     void dfs(int row, int col, vector<vector<int>>& vis,
              vector<vector<int>>& mat) {
         vis[row][col] = 1;
@@ -19,19 +18,61 @@ public:
     }
 
     int numEnclaves(vector<vector<int>>& board) {
-          int n = board.size(), m = board[0].size();
+        // int n = board.size(), m = board[0].size();
+        // vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        // for (int j = 0; j < m; j++) {
+
+        //     // first row
+        //     if (!vis[0][j] && board[0][j] == 1) {
+        //         dfs(0, j, vis, board);
+        //     }
+
+        //     // last row
+        //     if (!vis[n - 1][j] && board[n - 1][j] == 1) {
+        //         dfs(n - 1, j, vis, board);
+        //     }
+        // }
+
+        // for (int i = 0; i < n; i++) {
+
+        //     // first col
+        //     if (!vis[i][0] && board[i][0] == 1) {
+        //         dfs(i, 0, vis, board);
+        //     }
+
+        //     // last col
+        //     if (!vis[i][m - 1] && board[i][m - 1] == 1) {
+        //         dfs(i, m - 1, vis, board);
+        //     }
+        // }
+        // int cnt = 0;
+        // for (int i = 0; i < n; i++)
+        //     for (int j = 0; j < m; j++) {
+        //         if (!vis[i][j] && board[i][j] == 1) {
+        //             cnt++;
+        //         }
+        //     }
+
+        // return cnt;
+
+        // using multisource bfs
+        int n = board.size(), m = board[0].size();
         vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<int, int>> q;
 
         for (int j = 0; j < m; j++) {
 
             // first row
             if (!vis[0][j] && board[0][j] == 1) {
-                dfs(0, j, vis, board);
+                q.push({0, j});
+                  vis[0][j] = 1;
             }
 
             // last row
             if (!vis[n - 1][j] && board[n - 1][j] == 1) {
-                dfs(n - 1, j, vis, board);
+                q.push({n-1, j});
+                  vis[n-1][j] = 1;
             }
         }
 
@@ -39,22 +80,41 @@ public:
 
             // first col
             if (!vis[i][0] && board[i][0] == 1) {
-                dfs(i, 0, vis, board);
+                q.push({i, 0});
+                vis[i][0] = 1;
             }
 
             // last col
             if (!vis[i][m - 1] && board[i][m - 1] == 1) {
-                dfs(i, m - 1, vis, board);
+                q.push({i, m-1});
+                  vis[i][m-1] = 1;
             }
         }
-        int cnt=0;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && board[i][j] == 1) {
-                   cnt++;
+
+        int delR[] = {-1, 0, 1, 0}, delC[] = {0, 1, 0, -1};
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delR[i];
+                int ncol = col + delC[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    vis[nrow][ncol] == 0 && board[nrow][ncol] == 1) {
+                    q.push({nrow, ncol});
+                    vis[nrow][ncol] = 1;
                 }
             }
-            
-            return cnt;
+        }
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 1 && vis[i][j] == 0)
+                    cnt++;
+            }
+
+        return cnt;
     }
 };
