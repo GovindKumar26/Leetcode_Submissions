38a9aee60fe9642aea7ab85e633af92b04d5461f
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-12-18 16:21:14 +0530

    Time: 63 ms (13.46%), Space: 66.3 MB (11.21%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -21,23 +21,62 @@ public:
         return false;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // int V = graph.size();
+
+        // vector<int> vis(V, 0);     // Visited vector
+        // vector<int> pathVis(V, 0); // Path visited vector
+        // vector<int> safeNodes;
+        // vector<int> check(V, 0);
+
+        // for (int i = 0; i < V; i++) {
+        //     if (!vis[i]) {
+        //         dfsCheck(i, graph, vis, pathVis, check);
+        //     }
+        // }
+
+        // for (int i = 0; i < V; i++) {
+        //     if (check[i] == 1)
+        //         safeNodes.push_back(i);
+        // }
+        // return safeNodes;
+
+
+
+        // using toposort
+
         int V = graph.size();
+        vector<vector<int>>revGraph(V);
+        vector<int>indegree(V, 0);
 
-        vector<int> vis(V, 0);     // Visited vector
-        vector<int> pathVis(V, 0); // Path visited vector
-        vector<int> safeNodes;
-        vector<int> check(V, 0);
+        for(int i=0 ; i<V ; i++){
+            for(auto it : graph[i]){
+                revGraph[it].push_back(i);
+                indegree[i]++;
+            } 
+        }
 
-        for (int i = 0; i < V; i++) {
-            if (!vis[i]) {
-                dfsCheck(i, graph, vis, pathVis, check);
+        queue<int>q;
+        vector<int>safeNodes;
+
+        for(int i=0 ; i<V ; i++){
+            if(indegree[i]==0) {
+                q.push(i);
             }
         }
 
-        for (int i = 0; i < V; i++) {
-            if (check[i] == 1)
-                safeNodes.push_back(i);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safeNodes.push_back(node);
+
+            for(auto it : revGraph[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
         }
+
+        sort(safeNodes.begin(), safeNodes.end());
         return safeNodes;
+       
     }
 };
