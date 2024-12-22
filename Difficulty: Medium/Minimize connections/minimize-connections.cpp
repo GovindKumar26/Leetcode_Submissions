f2deda8b5823e03d084494df0fa021984cba865d
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-12-22 19:24:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize connections/minimize-connections.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class DisjointSet{
+    
+    public : 
+     vector<int> parent, rank, size;
+    
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        
+        for(int i=0 ; i<=n ; i++) parent[i] = i;
+    }
+    
+    int findParent(int node){
+        if(node==parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionByRank(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_v==ulp_u) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+      void unionBySize(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_v==ulp_u) return;
+        
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else if(size[ulp_v] < size[ulp_u]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution{
+    public:
+        int minimumConnections(int n , vector<vector<int>> &connections)
+        {
+           DisjointSet ds(n);
+           int countExtra = 0;
+           
+           for(auto it : connections){
+               int u = it[0], v = it[1];
+               if(ds.findParent(u)==ds.findParent(v)){
+                   countExtra++;
+               }
+               else{
+                   ds.unionBySize(u, v);
+               }
+           }
+           
+           int countC = 0;
+           
+           for(int i=0 ; i<n ; i++){
+               if(ds.parent[i]==i) countC++;
+           }
+           
+           int ans = countC - 1;
+           if(countExtra >= ans) return ans;
+           return -1;
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int m;
+        cin>>m;
+        vector<vector<int>> connections(m,vector<int> (2));
+        for(auto &j:connections)
+            cin>>j[0]>>j[1];
+        Solution s;
+        cout<<s.minimumConnections(n,connections)<<endl;
+    
+cout << "~" << "\n";
+}
+    return 0;
+}
+// } Driver Code Ends
