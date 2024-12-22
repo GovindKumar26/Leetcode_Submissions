1f527d7dfc644018346fe05a1e01177deed4ea5b
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-12-22 18:01:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -4,38 +4,133 @@ using namespace std;
 
 
 // } Driver Code Ends
+
+
+class DisjointSet{
+    vector<int> parent, rank, size;
+    
+    public : 
+    
+    DisjointSet(int n){
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        
+        for(int i=0 ; i<=n ; i++) parent[i] = i;
+    }
+    
+    int findParent(int node){
+        if(node==parent[node]) return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    void unionByRank(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_v==ulp_u) return;
+        if(rank[ulp_u] < rank[ulp_v]){
+            parent[ulp_u] = ulp_v;
+        }
+        
+        else if(rank[ulp_v] < rank[ulp_u]){
+            parent[ulp_v] = ulp_u;
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    
+      void unionBySize(int u, int v){
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        
+        if(ulp_v==ulp_u) return;
+        
+        if(size[ulp_u] < size[ulp_v]){
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else if(size[ulp_v] < size[ulp_u]){
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+        else{
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+    
+    
+};
+
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[]) {
-         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        //  priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
          
-         vector<int> vis(V, 0);
+        //  vector<int> vis(V, 0);
          
-         // wt, node
-         pq.push({0, 0});
-         int sum = 0;
+        //  // wt, node
+        //  pq.push({0, 0});
+        //  int sum = 0;
          
-         while(!pq.empty()){
-             int node = pq.top().second;
-             int wt = pq.top().first;
-             pq.pop();
+        //  while(!pq.empty()){
+        //      int node = pq.top().second;
+        //      int wt = pq.top().first;
+        //      pq.pop();
              
-             if(vis[node]==1) continue;
+        //      if(vis[node]==1) continue;
              
-             //add it to the mst
-             sum += wt;
-             vis[node] = 1;
-             for(auto it : adj[node]){
-                 int adjNode = it[0];
-                 int edgeW = it[1];
+        //      //add it to the mst
+        //      sum += wt;
+        //      vis[node] = 1;
+        //      for(auto it : adj[node]){
+        //          int adjNode = it[0];
+        //          int edgeW = it[1];
                  
-                 if(!vis[adjNode]) {
-                     pq.push({edgeW, adjNode});
-                 }
-             }
-         }
-         return sum;
+        //          if(!vis[adjNode]) {
+        //              pq.push({edgeW, adjNode});
+        //          }
+        //      }
+        //  }
+        //  return sum;
+        
+        
+        
+        // using dsu (needs edges)
+        
+        // converting adj to edges
+        vector<pair<int, pair<int, int>>> edges;
+        
+        for(int i=0 ; i<V ; i++){
+            for(auto it : adj[i]){
+                int adjNode = it[0];
+                int wt = it[1];
+                int node = i;
+                
+                edges.push_back({wt, {node, adjNode}});
+            }
+        }
+        
+        DisjointSet ds(V);
+        sort(edges.begin(), edges.end());
+        
+        int mstSum = 0;
+        
+        for(auto it : edges){
+            int wt = it.first;
+            int u = it.second.first;
+            int v = it.second.second;
+            
+            if(ds.findParent(u) != ds.findParent(v)){
+                mstSum += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+        return mstSum;
     }
 };
 
