8e2f7a6b0a9834688ea6e22b94728ee7b76b9b25
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2024-12-28 15:13:45 +0530

    Time: 0 ms (100%), Space: 12.2 MB (35.18%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        int k = n;
+        // int n = citations.size();
+        // int k = n;
+        // sort(citations.begin(), citations.end());
+
+        // while (k) {
+        //     int cnt = 0;
+        //     bool f = 0;
+        //  // for counting approach   // for(auto it : citations){
+        //     //     if(it>=k) cnt++;
+        //     // }
+        // // for sorting approach    for (int i = 0; i < n - k + 1; i++) {
+        //         if (citations[i] >= k) {
+        //             f = 1;
+        //             break;
+        //         }
+        //     }
+        //     if (f) {
+        //         break;
+        //     } else {
+        //         k--;
+        //     }
+        // }
+        // return k;
+
+
+         int n = citations.size();
         sort(citations.begin(), citations.end());
 
-        while (k) {
-            int cnt = 0;
-            bool f = 0;
-            // for(auto it : citations){
-            //     if(it>=k) cnt++;
-            // }
-            for (int i = 0; i < n - k + 1; i++) {
-                if (citations[i] >= k) {
-                    f = 1;
-                    break;
-                }
-            }
-            // if (cnt >= k)
-            //     break;
-            // else
-            //  k--;
-            // break;
-            if (f) {
-                break;
-            } else {
-                k--;
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
             }
         }
-        return k;
+
+        return 0;
     }
 };
