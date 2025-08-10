b64fbe7e56b3ae9ec80195675a999e3becf0657c
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2025-08-10 13:25:39 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+  
+  bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+         int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int limit = stalls[n - 1] - stalls[0];
+    for (int i = 1; i <= limit; i++) {
+        if (canWePlace(stalls, i, k) == false) {
+            return (i - 1);
+        }
+    }
+    return limit;
+    }
+};
