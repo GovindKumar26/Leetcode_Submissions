b8423b0d1cae0ef3d2b63fa94128cd49589a614b
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2025-08-10 09:49:16 +0530

    Time: 29 ms (21.81%), Space: 123.4 MB (5.39%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
-
-    bool check(vector<int>bloomDay, int mid, int m, int k){
+    bool check(vector<int> bloomDay, int mid, int m, int k) {
         int cnt = 0;
         int noOfBlooms = 0;
-        for(int i=0 ; i<bloomDay.size() ; i++){
-            if(bloomDay[i]<=mid){
+        for (int i = 0; i < bloomDay.size(); i++) {
+            if (bloomDay[i] <= mid) {
                 cnt++;
-            }
-            else{
-                noOfBlooms += cnt/k;
-                cnt=0;
+            } else {
+                noOfBlooms += cnt / k;
+                cnt = 0;
             }
         }
-        noOfBlooms += cnt/k;
-        return noOfBlooms>=m;
+        noOfBlooms += cnt / k;
+        return noOfBlooms >= m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long val = m*1LL * k*1LL;
-        if(bloomDay.size()<val) return -1;
+        long long val = m * 1LL * k * 1LL;
+        if (bloomDay.size() < val)
+            return -1;
         int ans;
 
         int mini = INT_MAX;
         int maxi = INT_MIN;
 
-        for(auto i : bloomDay){
-            if(i>maxi) maxi = i;
-            if(i<mini) mini = i;
+        for (auto i : bloomDay) {
+            if (i > maxi)
+                maxi = i;
+            if (i < mini)
+                mini = i;
         }
 
         int low = mini;
         int high = maxi;
 
-        while(low<=high){
-            int mid = (high-low)/2 + low;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
 
-            if(check(bloomDay, mid, m, k)){
+            if (check(bloomDay, mid, m, k)) {
                 ans = mid;
-                high = mid-1;
-            }
-            else {
-                low = mid+1;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return ans;
