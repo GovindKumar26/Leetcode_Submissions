c8ac397c5f7e602c1d036bcb43430c60d2d6986e
Govind Kumar <135205238+GovindKumar26@users.noreply.github.com>
2025-08-18 14:43:16 +0530

    Time: 0 ms (100%), Space: 10.9 MB (72.47%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        vector<int> countFreq(256, 0);
+
+        for (int i = 0; i < s.length(); i++) {
+            countFreq[s[i]]++;
+        }
+
+        priority_queue<pair<int, int>> pq; // Max-heap by default
+
+        string ans = "";
+
+        for (int i = 0; i < 256; i++) {
+            pq.push({countFreq[i], i});
+        }
+
+        while (!pq.empty()) {
+            char ele = pq.top().second ;
+            int freq = pq.top().first;
+            pq.pop();
+
+            while (freq--)
+                ans += ele;
+        }
+        return ans;
+
+        //  unordered_map<char, int> freq;  // Frequency map for all characters
+
+        // // Count frequencies
+        // for (char c : s) {
+        //     freq[c]++;
+        // }
+
+        // // Max-heap: pair<frequency, character>
+        // priority_queue<pair<int, char>> pq;
+        // for (auto &p : freq) {
+        //     pq.push({p.second, p.first});
+        // }
+
+        // string ans = "";
+
+        // // Build result string
+        // while (!pq.empty()) {
+        //     auto [count, ch] = pq.top();
+        //     pq.pop();
+
+        //     ans.append(count, ch);  // append 'ch' 'count' times
+        // }
+
+        // return ans;
+    }
+};
